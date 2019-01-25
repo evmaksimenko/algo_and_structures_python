8c7770df11c3772889560f4703a74a9a568fb74a
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-01-25 13:54:22 +0700

    Add lesson 5 files.

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,51 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import defaultdict
+from functools import reduce
+
+
+def annual_profit(l):
+    # Вычисляем годовую прибыль по квартальным прибылям
+    profit = reduce(lambda acc, x: acc * (1 + x / 100), [1] + l)
+    # Преобразуем в % и возвращаем
+    return (profit - 1) * 100
+
+
+company_number = int(input('Введите количество предприятий: '))
+companies = defaultdict(list)
+for i in range(company_number):
+    company = input(f'Введите название {i + 1} компании: ')
+    for quarter in range(4):
+        q = float(input(f'Введите прибыль в % за {quarter + 1} квартал: '))
+        companies[company].append(q)
+print()
+
+total = 0
+for c in companies.values():
+    # Вычисляем годовую прибыль по квартальным прибылям
+    total += annual_profit(c)
+# Средняя годовая прибыль среди всех предприятий
+avg = total / company_number
+
+print(f'Средняя годовая прибыль предприятий {avg:5.2f}')
+print()
+
+print('Предприятия с прибылью выше среднего:')
+is_find = False
+for c in companies.keys():
+    if annual_profit(companies[c]) > avg:
+        is_find = True
+        print(c)
+if not is_find:
+    print('отсутствуют')
+
+print('Предприятия с прибылью ниже среднего:')
+is_find = False
+for c in companies.keys():
+    if annual_profit(companies[c]) < avg:
+        is_find = True
+        print(c)
+if not is_find:
+    print('отсутствуют')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,51 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+"""
+#1
+class Hex:
+    @staticmethod
+    def to_num(digits_list):
+        return int(''.join(digits_list), 16)
+
+    @staticmethod
+    def to_hex(num):
+        return Hex(hex(num)[2:])
+
+    def __init__(self, value):
+        self.hex_digits = [ch for ch in (value.upper())]
+
+    def __add__(self, other):
+        return Hex.to_hex(Hex.to_num(self.hex_digits) + Hex.to_num(other.hex_digits))
+
+    def __mul__(self, other):
+        return Hex.to_hex(Hex.to_num(self.hex_digits) * Hex.to_num(other.hex_digits))
+
+    def __str__(self):
+        return str(self.hex_digits)
+
+
+a = Hex('A2')
+print(a)
+b = Hex('C4F')
+print(b)
+c = a + b
+print(c)
+d = a * b
+print(d)
+"""
+
+"""
+# 2
+import string
+
+a = [ch.upper() for ch in input("Введите первое число: ") if ch in string.hexdigits]
+b = [ch.upper() for ch in input("Введите второе число: ") if ch in string.hexdigits]
+print(a)
+print(b)
+c = [ch.upper() for ch in hex(int(''.join(a), 16) + int(''.join(b), 16))[2:]]
+d = [ch.upper() for ch in hex(int(''.join(a), 16) * int(''.join(b), 16))[2:]]
+print(f'Сумма чисел равна {c}')
+print(f'Произведение чисел равно {d}')
+"""
