1d211bdc3fddeafd601ac305a5c32ae500c8b150
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-01-27 18:42:58 +0700

    Add lesson 2 files.

--- Lesson_2/2.py
+++ Lesson_2/2.py
@@ -1,5 +1,26 @@
 """
-2.	Посчитать четные и нечетные цифры введенного натурального числа.
-Например, если введено число 34560, то у него 3 четные цифры
-(4, 6 и 0) и 2 нечетные (3 и 5).
+2. Посчитать четные и нечетные цифры введенного натурального числа. Например, если введено число 34560, то у него 3
+четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
 """
+
+# 1
+n = input("Введите число: ")
+digits = list(map(int, n))
+odd_digits = list(filter(lambda x: x % 2 == 1, digits))
+even_digits = list(filter(lambda x: x % 2 == 0, digits))
+print(f"Нечётных чисел {len(odd_digits)} {odd_digits}")
+print(f"Чётных чисел {len(even_digits)} {even_digits}")
+
+# 2
+n = int(n)
+odd_digits = []
+even_digits = []
+while n > 0:
+    r = n % 10
+    n //= 10
+    if r % 2 == 0:
+        even_digits.append(r)
+    else:
+        odd_digits.append(r)
+print(f"Нечётных чисел {len(odd_digits)} {odd_digits}")
+print(f"Чётных чисел {len(even_digits)} {even_digits}")
