862d5d5a38719ac9e9471377699cd8180901e55b
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-01-30 16:29:13 +0700

    Lesson 7. Add exercise 2 file.

--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,37 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+"""
+Windows 8.1 x64
+python 2.7 x32
+"""
+from pympler import asizeof
+# Класс с использование __slots__
+class MyClassWithSlots:
+    __slots__ = 'X', 'Y', 'data'
+
+    def __init__(self):
+        self.X = 0
+        self.Y = 0
+        self.data = "New point"
+# Обычный класс
+class MyClassWithoutSlots:
+    def __init__(self):
+        self.X = 0
+        self.Y = 0
+        self.data = "New point"
+
+
+a = MyClassWithSlots()
+print(f'Размер экземпляра класса с использованием __slots__ равен {asizeof.asizeof(a)} байт') # 96 байт
+print(f'Словарь аттрибутов класса: {MyClassWithSlots.__dict__}')
+try:
+    print(f'Словарь аттрибутов экземпляра: {a.__dict__}')
+except AttributeError:
+    print('У экземпляра a отсутствует __dict__')
+print()
+
+b = MyClassWithoutSlots()
+print(f'Размер экземпляра класса без использования __slots__ равен {asizeof.asizeof(b)} байт') # 256 байт
+print(f'Словарь аттрибутов класса: {MyClassWithoutSlots.__dict__}')
+print(f'Словарь аттрибутов экземпляра: {b.__dict__}')
