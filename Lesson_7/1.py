d8c54bacc5eab8b3c89d0812642e6af7fc4e1810
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-02-01 15:27:29 +0700

    Add exercise 1 of lesson 7.

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,22 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+from random import randrange
+
+
+def bubble_sort(array):
+    a = array[:]
+    for i in range(len(a) - 1):
+        for j in range(len(a) - i - 1):
+            if a[j] < a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return a
+
+
+N = 20
+arr = [randrange(-100, 100) for _ in range(N)]
+
+print('Исходный массив:')
+print(arr)
+print('Отсортированный массив по убыванию:')
+print(bubble_sort(arr))
