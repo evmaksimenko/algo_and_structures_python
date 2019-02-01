5252f7d017f1346ded3c420ff3e97b542009bc98
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-02-01 16:32:09 +0700

    Add exercise 2 lesson 7 file.

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,32 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+from random import random
+
+
+def merge_sort(a):
+    if len(a) <= 1:
+        return a
+    left = merge_sort(a[:len(a)//2])
+    right = merge_sort(a[len(a)//2:])
+    merge = []
+    while True:
+        if not left and not right:
+            break
+        if not left:
+            merge.append(right.pop(0))
+        elif not right:
+            merge.append(left.pop(0))
+        else:
+            if left[0] <= right[0]:
+                merge.append(left.pop(0))
+            else:
+                merge.append(right.pop(0))
+
+    return merge
+
+
+N = 10
+arr = [round(random() * 50, 3) for _ in range(N)]
+print(arr)
+print(merge_sort(arr))
